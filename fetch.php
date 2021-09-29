93667eb900216797f18aacfcf52bd34d0cbd868d
MKody <im@kdy.ch>
2021-09-29 15:39:26 +0000

    Use airingSchedule to save current season

--- fetch.php
+++ fetch.php
@@ -14,6 +14,26 @@ foreach (range(2020, 2030) as $year) {
     $lists[] = $year . ' Fall';
 }
 
+// Find current season
+$query = file_get_contents(__DIR__ . '/airingSeasons.graphql');
+$variables = [
+    "now" => strtotime('now')
+];
+$response = graphql('https://graphql.anilist.co', $query, json_encode($variables));
+$season = [
+    "int" => 0,
+    "string" => ''
+];
+foreach ($response->data->Page->airingSchedules as $s) {
+    if ($s->media->seasonInt > $season['int']) {
+        $season = [
+            "int" => $s->media->seasonInt,
+            "string" => $s->media->seasonYear . '-' . ucwords(strtolower($s->media->season))
+        ];
+    }
+}
+file_put_contents(__DIR__ . '/currentSeason.txt', $season['string']);
+
 // Create object where our data is saved
 $j = [];
 
